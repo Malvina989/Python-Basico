52af6f18be15c184e22369fef090b9ed26bf80e9
Malvina989 <64342195+Malvina989@users.noreply.github.com>
2022-06-28 20:01:12 -0300

    Add files via upload

---
+++ adivinar_num_secreto.py
@@ -0,0 +1,21 @@
+﻿secret_number = 777
+
+print(
+"""
++==================================+
+| ¡Bienvenido a mi juego, muggle!  |
+| Introduce un número entero       |
+| y adivina qué número he          |
+| elegido para ti.                 |
+| Entonces,                        |
+| ¿Cuál es el número secreto?      |
++==================================+
+""")
+number = int(input("Arriesgue número"))
+while number != secret_number:
+    print("Ja,ja Estás en mi bucle")
+    number = int(input("Arriesgue número"))
+if number == secret_number:
+    print(secret_number)
+    print("¡Bien hecho!")
+    
